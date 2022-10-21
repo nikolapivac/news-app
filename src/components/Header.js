3c917ff8116b676d713c0aeaf977e36f6c62c10f
nikolapivac <npivac00@fesb.hr>
2022-10-21 10:51:51 +0200

    Added Mobile elements (menus)

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,23 @@
 import React from "react";
 import Search from "./Search";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 const Header = ({searchArticles}) => {
     return (
         <div className="header">
-            <div className="logo">
-                <h1 className="logo_1">My</h1>
-                <h1 className="logo_2">News</h1>
+            <div className="header_top">
+                <div className="logo">
+                    <h1 className="logo_1">My</h1>
+                    <h1 className="logo_2">News</h1>
+                </div>
+                <FontAwesomeIcon className="bars" icon={faBars} />
             </div>
             <Search searchArticles={searchArticles} />
+            <div className="mobile_menu">
+                <button className="mobtn active">Featured</button>
+                <button className="mobtn">Latest</button>
+            </div>
         </div>
     )
 }
